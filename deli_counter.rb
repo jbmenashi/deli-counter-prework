ee78f1ca554fb400e3f93bf51e73eedc5f133a5a
Jacob Menashi <jbmenashi@gmail.com>
2018-09-28 14:58:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
   else
-    let line_addition = []
+    line_addition = []
     katz_deli.each do |name|
       line.addition.push("#{katz_deli.index(name) + 1}. #{name}")
     end
