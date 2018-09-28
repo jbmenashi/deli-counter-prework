88e496be2d20d2d41b6ec86ad825dd08d6b85f68
Jacob Menashi <jbmenashi@gmail.com>
2018-09-28 15:02:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 def line(deli)
   if deli.size == 0 
     puts "The line is currently empty."
-  else
-    line_addition = []
-    deli.each do |name|
-      line.addition.push("#{deli.index(name) + 1}. #{name}")
-    end
-    "The line is currently: #{line_addition}."
-  end
+    puts "Hello"
+  #   line_addition = []
+  #   deli.each do |name|
+  #     line.addition.push("#{deli.index(name) + 1}. #{name}")
+  #   end
+  #   "The line is currently: #{line_addition}."
+  # end
 end
