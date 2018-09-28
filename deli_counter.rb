afc30d35a24ef1b74fb7b52436c36e199c31898a
Jacob Menashi <jbmenashi@gmail.com>
2018-09-28 15:19:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,15 @@
+def line(the_line)
+  if the_line.size == 0 
+    puts "The line is currently empty."
+  else
+    line_string = ""
+    the_line.each_with_index do |name, idx|
+      line_string << " #{idx + 1}. #{name}"
+    end
+    puts "The line is currently:#{line_string}"
+  end
+end
+
 def take_a_number(line, name)
   line << name
   puts "Welcome, #{name}. You are number #{line.size} in line."
@@ -8,17 +20,5 @@ def now_serving(line)
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{line.shift}."
-  end
-end
-
-def line(the_line)
-  if the_line.size == 0 
-    puts "The line is currently empty."
-  else
-    line_string = ""
-    the_line.each_with_index do |name, idx|
-      line_string << " #{idx + 1}. #{name}"
-    end
-    puts "The line is currently:#{line_string}"
   end
 end
