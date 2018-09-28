0a81703e9f7653626c9bc1c281acd80329595e31
Jacob Menashi <jbmenashi@gmail.com>
2018-09-28 14:50:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(katz_deli)
   if katz_deli.size == 0 
-    "The line is currently empty."
+    puts "The line is currently empty."
   else
   line << name
   "Welcome, #{name}. You are number #{katz_deli.size} in line."
