1e5c8ff7db3442c237cc5dd6dba33910929ace75
Jacob Menashi <jbmenashi@gmail.com>
2018-09-28 15:07:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,3 @@
-def line(deli)
-  if deli.size == 0 
-    puts "The line is currently empty."
-  else
-    puts "#{deli}"
-    # line_addition = []
-    # deli.each do |name|
-    #   line.addition.push("#{deli.index(name) + 1}. #{name}")
-    # end
-    # puts "The line is currently: #{line_addition}."
-  end
-end
+def take_a_number(line, name)
+  line << name
+  puts "Welcome, #{name}. You are number #{line.size} in line."
